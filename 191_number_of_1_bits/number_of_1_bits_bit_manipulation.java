f1de8faaecb008362cf8a42dae2faeb28d6a2f1a
Yunmo Chen <cyunmo@gmail.com>
2018-04-09 18:07:12 +0800

    Add 191
    Add 191

--- 191_number_of_1_bits/number_of_1_bits_bit_manipulation.java
+++ 191_number_of_1_bits/number_of_1_bits_bit_manipulation.java
@@ -1,5 +1,9 @@
 public class Solution {
     // you need to treat n as an unsigned value
+
+	// n & (n - 1)
+	// has very interesting feature
+
     public int hammingWeight(int n) {
         int count = 0;
         
